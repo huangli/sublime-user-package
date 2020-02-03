e4438135eee113cb7f443030f6fc40648bbab817
huangli <ekinasm@gmail.com>
2020-02-03 11:42:38 +0800

    test

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+####################################
+#
+# Backup sublime User folder to github
+#
+####################################
+
+backup_folder = $HOME/.config/sublime-text-3/Packages/User
+cd "$backup_folder"
+
