0f78ad640b189270337824ae60a55497cdf3642e
huangli <ekinasm@gmail.com>
2020-02-08 09:59:45 +0800

    Ubuntu wiki 1.0

--- backup.sh
+++ backup.sh
@@ -8,17 +8,6 @@
 cd $HOME/.config/sublime-text-3/Packages/User
 git add .
 git commit -m "$(date +%Y-%m-%d) backup sublime"
-git push origin master > sublime_backup.log
+git push origin master
 
 
-####################################
-#
-# Backup zshrc
-#
-####################################
-
-cd $HOME/
-git add .zshrc
-git add README.md
-git commit -m "$(date +%Y-%m-%d) backup zshrc"
-git push origin maste > zshrc_backup.log
